ea65a7a9df6f87f2b1abb92450258ef2713fe799
akishore <abhikish2009@gmail.com>
2015-10-19 14:14:45 +0530

    alignment attempt 2

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ game.state.start("Main");
 		plane.width = 80;
 		plane.anchor.set(0.5,0.5);
 		
-		startText = game.add.bitmapText((innerWidth/4), 330, "SFComic", "Touch anywhere to start game", 36);
+		startText = game.add.bitmapText((innerWidth/3), 330, "SFComic", "Touch anywhere to start game", 36);
 		
 		game.input.onDown.add(startGame, this);
  }
